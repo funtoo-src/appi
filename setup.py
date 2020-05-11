a483a4feef06308dfc024090ba8f5a4ac8effd28
Daniel Robbins <drobbins@funtoo.org>
2020-05-11 13:31:53 -0600

    Remove __init__.py from test.

--- setup.py
+++ setup.py
@@ -1,40 +1,22 @@
-# -*- coding: utf-8 -*-
-# Distributed under the terms of the GNU General Public License v2
-from pathlib import Path
-import re
-from setuptools import setup, find_packages
+import setuptools
 
-BASE_DIR = Path(__file__).parent
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
 
-with (BASE_DIR / 'README.rst').open(encoding='utf-8') as readme:
-    README = readme.read()
-
-with (BASE_DIR / 'appi' / '__init__.py').open(encoding='utf-8') as init_file:
-    pattern = '''^\s*__version__\s*=\s*["']([0-9.]+)["']\s*$'''
-    VERSION = re.search(pattern, init_file.read(), re.M).group(1)
-
-setup(
-    name='appi',
-    version=VERSION,
-    packages=find_packages(exclude=['test']),
-    include_package_data=True,
-    license='GPL-2',
-    description="Another Portage Python Interface",
-    long_description=README,
-    url='https://gihtub.com/apinsard/appi',
-    author="Antoine Pinsard",
-    author_email='antoine.pinsard@gmail.com',
+setuptools.setup(
+    name="appi",
+    version="0.2.4",
+    author="Daniel Robbins, Antoine Pinsard",
+    author_email="drobbins@funtoo.org",
+    description="This is a python module created by Antoine Pinsard that powers ego query commands.",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://code.funtoo.org/bitbucket/projects/CORE/repos/appi/browse",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
-        'Operating System :: POSIX :: Linux'
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3 :: Only',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: System :: Systems Administration',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: POSIX :: Linux",
     ],
+    python_requires='>=3.7',
+    packages=setuptools.find_packages()
 )
